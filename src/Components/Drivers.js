2adb881c0b4581367f0a63ed43674f17c765e3f6
Eric Nelson <ernelson12o0@gmail.com>
2023-06-14 10:10:31 -0400

    add functionality to drivers filter

--- src/Components/Drivers.js
+++ src/Components/Drivers.js
@@ -5,6 +5,32 @@ function Drivers({ drivers, setDrivers }) {
 
     const [selectedOrder, setSelectedOrder] = useState('')
 
+    useEffect(() => {
+        function filterDrivers() {
+            let sortedDrivers;
+   
+            if(selectedOrder === 'Team') {
+                sortedDrivers = [...drivers].sort((a, b) => a.team.localeCompare(b.team));
+            } else if (selectedOrder === "Standing") {
+                sortedDrivers = [...drivers].sort((a, b) => b.standing - a.standing);
+            } else {
+                sortedDrivers = drivers;
+            }
+
+
+            setDrivers(sortedDrivers);
+        }
+
+
+        filterDrivers();
+    }, [selectedOrder]);
+
+
+    function handleOrderChange(e) {
+            setSelectedOrder(e.target.value);
+    }
+
+
     return(
     <div>
         <img className="drivers-header-img" src="https://i.guim.co.uk/img/media/cefc0674922217b6e0d646d338f08412ef9b9ff5/54_207_4975_2985/master/4975.jpg?width=1300&quality=45&dpr=2&s=none" />
