3fd19eb286711a053d9e408556095cdffb621bdf
Eric Nelson <ernelson12o0@gmail.com>
2023-06-14 09:50:26 -0400

    import files to App.js

---
+++ src/Components/App.js
@@ -0,0 +1,8 @@
+
+import React, { useEffect, useState } from "react";
+import {Switch, Route} from 'react-router-dom'
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Teams from './Teams';
+import Drivers from './Drivers';
+import Tracks from './Tracks';
