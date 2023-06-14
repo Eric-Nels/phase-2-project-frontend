b4dc67f2873326f623a10f8512686c2b4d90c4cb
Eric Nelson <ernelson12o0@gmail.com>
2023-06-14 10:09:43 -0400

    add filter options to drivers

--- src/Components/Drivers.js
+++ src/Components/Drivers.js
@@ -3,10 +3,18 @@ import '../Styles/Drivers.css'
 
 function Drivers({ drivers, setDrivers }) {
 
+    const [selectedOrder, setSelectedOrder] = useState('')
+
     return(
     <div>
         <img className="drivers-header-img" src="https://i.guim.co.uk/img/media/cefc0674922217b6e0d646d338f08412ef9b9ff5/54_207_4975_2985/master/4975.jpg?width=1300&quality=45&dpr=2&s=none" />
-        
+        <div>
+            <select value={selectedOrder} onChange={handleOrderChange}>
+                <option value=''>Select Order</option>
+                <option value='Team'>Teams</option>
+                <option value='Standing'>Standing</option>
+            </select>
+        </div>      
         <div className="drivers-container">
             <h1>Drivers</h1>
             <div className="drivers-grid">
