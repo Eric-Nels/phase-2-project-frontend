ce8ea516f109f34ba6e8d1f97c18ebd41304444d
Eric Nelson <ernelson12o0@gmail.com>
2023-06-14 10:18:26 -0400

    add tracks form

--- src/Components/Tracks.js
+++ src/Components/Tracks.js
@@ -6,7 +6,30 @@ function Tracks({tracks, setTracks}) {
     return(
         <div className="tracks-page">
             <h1>Tracks</h1>
-        
+            <form className="NewTrack" onSubmit={handleSubmit}>
+                <label> Track Location:
+                    <input type="text"
+                    name="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    />
+                </label>
+                <label> Image Url:
+                    <input type="text"
+                    name="image"
+                    value={image}
+                    onChange={(e) => setImage(e.target.value)}
+                    />
+                </label>
+                <label> Winner:
+                    <input type="text"
+                    name="winner"
+                    value={winner}
+                    onChange={(e) => setWinner(e.target.value)}
+                    />
+                </label>
+                <button type="Submit">Submit</button>
+            </form>
             <div className="tracks-container">
             {tracks.map((track)=> {
             return(
