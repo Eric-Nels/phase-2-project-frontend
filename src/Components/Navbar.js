75a2b732670e6ae5d764d40b03e4109f5159ce38
Eric Nelson <ernelson12o0@gmail.com>
2023-06-14 10:20:30 -0400

    create navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,4 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import '../Styles/Navbar.css'
 
+function Navbar() {
+  return(
+      <div className="navbar">
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/teams'>Teams</NavLink>
+        <NavLink to='/drivers'>Drivers</NavLink>
+        <NavLink to='/tracks'>Tracks</NavLink>
+      </div>
+    );
+};
+
+
 export default Navbar;
