78f30f6dce6417cb84e9acb4b9150cb387680821
Eric Nelson <ernelson12o0@gmail.com>
2023-06-14 10:17:40 -0400

    iterate through tracks data and display to DOM

--- src/Components/Tracks.js
+++ src/Components/Tracks.js
@@ -1,6 +1,26 @@
 import React, { useState } from "react";
 import "../Styles/Tracks.css"
 
+function Tracks({tracks, setTracks}) {
+   
+    return(
+        <div className="tracks-page">
+            <h1>Tracks</h1>
+        
+            <div className="tracks-container">
+            {tracks.map((track)=> {
+            return(
+                <div className="track-card" key={track.id}>
+                    <h2>{track.name}</h2>
+                    <img src={track.image} />
+                    <p>{track.winner}</p>
+                </div>
+            )
+        })}
+            </div>
+        </div>
+    )
+};
 
 
 export default Tracks;
